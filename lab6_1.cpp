398c109920a5febd4c8b64318efdddc30fba0396
PremKuyRai <koravit_dilok@cmu.ac.th>
2025-12-09 16:22:50 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int Even = 0 ,Odd = 0 , number;
+    while (1){
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number==0){
+            break;
+        }
+            if(number%2==0){
+                Even = Even+1;
+            }else{
+                Odd = Odd+1;
+            }
+
+    }
+    cout << "#Even numbers = "<<Even;
+    cout << "\n#Odd numbers = "<<Odd;
     return 0;
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double deg2rad(double deg){
+    double rad = deg*M_PI/180;
+    return rad;
+}
+
+double rad2deg(double rad){
+    double deg = rad*180/M_PI;
+    return deg;
+}
+
+double findXComponent(double l1, double l2, double a1, double a2){
+    double Xcomp = l1*cos(a1) + l2*cos(a2);
+    return Xcomp;
+}
+
+double findYComponent(double l1, double l2, double a1, double a2){
+    double Ycomp = l1*sin(a1) + l2*sin(a2);
+    return Ycomp;
+}
+
+double pythagoras(double xcomp,double ycomp){
+    double pythagoras = sqrt(pow(xcomp,2)+pow(ycomp,2));
+    return pythagoras;
+}
+
+void showResult(double result_vec_length,double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << result_vec_length;
+    cout << "\nDirection of the resultant vector (deg) = " << result_vec_direction;
+    cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
+
 int main(){
+
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
     cin >> l1;
+
     cout << "Enter direction of the first vector (deg): ";
     cin >> a1;
+
     cout << "Enter length of the second vector: ";
     cin >> l2;
+
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
     
